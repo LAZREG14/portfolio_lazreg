4c30985e151df3b0f675ed8b5b72b754ab97d8d6
LAZREG Mustapha <68035640+LAZREG14@users.noreply.github.com>
2024-01-29 14:25:58 +0100

    Update index.js

--- pages/cv/index.js
+++ pages/cv/index.js
@@ -4,7 +4,7 @@ const PDFPage = () => {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
       {/* Utilisation de l'iframe pour afficher le PDF avec styles CSS */}
-      <iframe src="/LAZREG_DS_CV.pdf" width="100%" height="77%" style={{ border: 'none' }}></iframe>
+      <iframe src="/CV_DS_LAZREG_.pdf" width="100%" height="77%" style={{ border: 'none' }}></iframe>
     </div>
   );
 };
